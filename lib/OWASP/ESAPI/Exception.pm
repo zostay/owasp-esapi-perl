16452c21fdc6fadab7a1897756b6ca39824b9197
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-09-05 00:09:44 -0500

    Adding the exception class

---
+++ lib/OWASP/ESAPI/Exception.pm
@@ -0,0 +1,19 @@
+package OWASP::ESAPI::Exception;
+use Moose;
+use Sub::Exporter::Util qw( curry_method );
+use Sub::Exporter -setup => {
+    exports => {
+        throw         => curry_method,
+        new_exception => curry_method('new'),
+    },
+    groups => {
+        defaults => [ qw( throw ) ],
+    },
+};
+
+with qw(
+    Throwable::X
+    StackTrace::Auto
+);
+
+__PACKAGE__->meta->make_immutable;
