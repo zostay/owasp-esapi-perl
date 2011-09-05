c7dcdf3d94a5e2e7f661be94ac83e002a0145a7a
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-09-04 23:00:40 -0500

    Adding the JavaScript codec

---
+++ lib/OWASP/ESAPI/Codec/JavaScriptCodec.pm
@@ -0,0 +1,46 @@
+package OWASP::ESAPI::Codec::JavaScriptCodec;
+use Moose;
+
+extends 'OWASP::ESAPI::Codec';
+
+use List::MoreUtils qw( any );
+
+sub encode_character {
+    my ($self, $immune, $c) = @_;
+
+    # immune: as-is
+    return $c if any { $c eq $_ } @$immune;
+
+    # alphanumeric: as-is
+    return $c if $c =~ /[a-zA-Z0-9]/;
+    
+    my $o = ord($c);
+
+    # ord(c) < 256: \xFF or whatever
+    return sprintf '\\x%02x', $o if $o < 256;
+
+    # ord(c) >= 256: \uFFFF or whatever
+    return sprintf '\\u%04x', $o;
+}
+
+sub decode_character {
+    my ($self, $input) = @_;
+
+    return "\b" if $$input =~ s{^\\b}{};
+    return "\t" if $$input =~ s{^\\t}{};
+    return "\n" if $$input =~ s{^\\n}{};
+    return "\v" if $$input =~ s{^\\v}{};
+    return "\f" if $$input =~ s{^\\f}{};
+    return "\r" if $$input =~ s{^\\r}{};
+    return '"'  if $$input =~ s{^\\"}{};
+    return "'"  if $$input =~ s{^\\'}{};
+    return "\\" if $$input =~ s{^\\\\}{};
+
+    return chr(hex($1)) if $$input =~ s{^\\x([a-fA-F0-9]{2})}{};
+    return chr(hex($1)) if $$input =~ s{^\\u([a-fA-F0-9]{4})}{};
+    return chr(oct($1)) if $$input =~ s{^\\([0-7]{3})}{};
+
+    return substr $$input, 0, 1, '';
+}
+
+__PACKAGE__->meta->make_immutable;
